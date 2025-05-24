2a8adb8c251edad195635242fcb542cbbc401f8d
William McDermott <williammcdermott@MacBookPro.lan>
2025-05-24 17:02:30 -0500

    Add CustomMap class

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,10 @@
 /// <reference types="@types/google.maps" />
 import { User } from './User';
 import { Company } from './Company';
+import { CustomMap } from './CustomMap';
 
 const initMap = () => {
-  new google.maps.Map(document.getElementById('map') as HTMLElement, {
-    zoom: 1,
-    center: {
-      lat: 0,
-      lng: 0,
-    },
-  });
+    new CustomMap('map');
 };
 
 (window as any).initMap = initMap
