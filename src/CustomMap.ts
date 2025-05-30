b21145cda1d2f16c91d74c62414a86e3958902ff
William McDermott <williammcdermott@MacBookPro.lan>
2025-05-29 21:20:17 -0500

    Generate marker content from user and company data

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -2,7 +2,8 @@ interface Mappable {
     location: {
         lat: number;
         lng: number;
-    }
+    };
+    markerContent(): string;
 }
 
 export class CustomMap {
@@ -33,7 +34,7 @@ export class CustomMap {
 
     marker.addListener('gmp-click', () => {
         const infoWindow = new google.maps.InfoWindow({
-            content: 'Hi there!'
+            content: mappable.markerContent()
         });
         infoWindow.open(this.googleMap, marker);
     });
